6a939778bf573df0398cfac10376b96275fc81ad
Quentin Champenois <quentin.champenois7@gmail.com>
2025-07-23 12:45:10 +0200

    fix: Signature refactors

--- app/app/utils.py
+++ app/app/utils.py
@@ -4,7 +4,7 @@ from .models import Host
 
 
 def increase_spam_count(h: Host, r: redis.Redis, spam_limit: int, spam_period_limit: int) -> True | False:
-    """Increase the host spam count and send webhook request"""
+    """Increase the host spam count and returns true if limit exceeded"""
     r.incr(h.total_redis_key())
     if r.get(h.period_redis_key()) is None:
         r.set(h.period_redis_key(), 1)
@@ -16,7 +16,7 @@ def increase_spam_count(h: Host, r: redis.Redis, spam_limit: int, spam_period_li
     return current_period_count > spam_limit
 
 
-def send_webhook_notification(h: Host, webhook: tuple, r: redis.Redis, **kwargs) -> True | False:
+def send_webhook_notification(h: Host, webhook: tuple, r: redis.Redis, **kwargs) -> None:
     """Send webhook notification"""
     limit = kwargs.get("limit")
     total_count = kwargs.get("total_count")
